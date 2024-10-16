f4139bf0037adc9a96f3bcf0f6b5bde9d0c4b863
SiddeshMandhare <sidmandhare3@gmail.com>
2024-10-16 23:22:28 +0530

    hello

--- src/main/java/com/example/Fusion_ms/AWS.java
+++ src/main/java/com/example/Fusion_ms/AWS.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AWS {
     @GetMapping("/AWS")
-    public String getData() {return  "Welcome to Fusion-AWS class" ; }
+    public String getData() {return  "Welcome to Fusion-AWS class with EC2, S3, Lambda" ; }
 }
