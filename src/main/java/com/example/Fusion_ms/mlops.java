48d1f506098c16d330b397e5ee5df7b25b302b88
SiddeshMandhare <sidmandhare3@gmail.com>
2024-10-16 20:40:17 +0530

    adding mlops clas

--- src/main/java/com/example/Fusion_ms/mlops.java
+++ src/main/java/com/example/Fusion_ms/mlops.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class mlops {
     @GetMapping("/mlops")
-    public String getData() {return  "Welcome to Fusion-mlops class" ; }
+    public String getData() {return  "Welcome to Fusion mlops class" ; }
 }
 
